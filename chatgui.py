146c595a684bc99d378f5aa9f89d64274156881c
Ataches <jussanchezm@correo.udistrital.edu.co>
2021-10-07 18:19:10 -0500

    translated

--- chatgui.py
+++ chatgui.py
@@ -1,37 +1,44 @@
-import nltk, json, random, pickle
-from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
-import pickle
+import json
+import nltk
 import numpy as np
+import pickle
+import random
+from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import load_model
+
+lemmatizer = WordNetLemmatizer()
+
 model = load_model('chatbot_model.h5')
 intents = json.loads(open('intents.json').read())
-words = pickle.load(open('words.pkl','rb'))
-classes = pickle.load(open('classes.pkl','rb'))
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
 
-# preprocessamento input utente
+
+# User input processing
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
-# creazione bag of words
+
+# bag of words creation
 def bow(sentence, words, show_details=True):
     sentence_words = clean_up_sentence(sentence)
-    bag = [0]*len(words)
+    bag = [0] * len(words)
     for s in sentence_words:
-        for i,w in enumerate(words):
+        for i, w in enumerate(words):
             if w == s:
                 bag[i] = 1
                 if show_details:
-                    print ("found in bag: %s" % w)
-    return(np.array(bag))
+                    print("found in bag: %s" % w)
+    return (np.array(bag))
 
-def calcola_pred(sentence, model):
-    p = bow(sentence, words,show_details=False)
+
+def prediction_calculation(sentence, model):
+    p = bow(sentence, words, show_details=False)
     res = model.predict(np.array([p]))[0]
-    ERROR_THRESHOLD = 0.25
-    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    error_threshold = 0.25  # ERROR_THRESHOLD
+    results = [[i, r] for i, r in enumerate(res) if r > error_threshold]
     # sort by strength of probability
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
@@ -39,24 +46,28 @@ def calcola_pred(sentence, model):
         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
     return return_list
 
-def getRisposta(ints, intents_json):
+
+def get_response(ints, intents_json):
     tag = ints[0]['intent']
     list_of_intents = intents_json['intents']
-    for i in list_of_intents:
-        if(i['tag']== tag):
-            result = random.choice(i['responses'])
+    result = list_of_intents[3]['responses']
+    for intent in list_of_intents:
+        if intent['tag'] == tag:
+            result = random.choice(intent['responses'])
             break
     return result
 
-def inizia(msg):
-    ints = calcola_pred(msg, model)
-    res = getRisposta(ints, intents)
-    return res
 
-utente = ''
-print('Benvenuto! Per uscire, scrivi "Esci"')
+def start(msg):
+    ints = prediction_calculation(msg, model)
+    return get_response(ints, intents)
 
-while utente != 'esci':
-    utente = str(input(""))
-    res = inizia(utente)
-    print('AI:' + res)
+
+if __name__ == "__main__":
+    user_response = ''
+    print('Welcome! Write "end" to stop')
+
+    while user_response != 'end':
+        user_response = str(input(""))
+        AI_response = start(user_response)
+        print('AI:' + AI_response)
--- train_bot.py
+++ train_bot.py
@@ -1,4 +1,5 @@
 import nltk
+
 nltk.download('punkt')
 nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer
@@ -8,7 +9,7 @@ import pickle
 
 import numpy as np
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Conv2D, Flatten, Dense, Dropout
+from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.optimizers import SGD
 
 lemmatizer = WordNetLemmatizer()
@@ -20,15 +21,15 @@ data_file = open('intents.json').read()
 intents = json.loads(data_file)
 print(intents)
 
-# intents: gruppi di conversazioni-tipo
-# patterns: possibili interazioni dell'utente
+# intents: typical conversation groups
+# patterns: possible user interactions
 for intent in intents['intents']:
     for pattern in intent['patterns']:
 
-        # tokenizzo ogni parola
+        # tokenize every word
         w = nltk.word_tokenize(pattern)
         words.extend(w)
-        # aggiungo all'array documents
+        # add to the documents array
         documents.append((w, intent['tag']))
 
         # adding classes to our class list
@@ -37,20 +38,20 @@ for intent in intents['intents']:
 
 words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
 
-pickle.dump(words, open('words.pkl','wb'))
-pickle.dump(classes, open('classes.pkl','wb'))
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
 
-# preparazione per l'addestramento della rete
+# preparation for network training
 training = []
 output_empty = [0] * len(classes)
 for doc in documents:
     # bag of words
     bag = []
-    # lista di tokens
+    # tokens list
     pattern_words = doc[0]
-    # lemmatizzazione dei token
+    # lemmatization of tokens
     pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
-    # se la parola matcha, inserisco 1, altriment 0
+    # if the word matches, insert 1, otherwise 0
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
 
@@ -60,11 +61,11 @@ for doc in documents:
     training.append([bag, output_row])
 
 training = np.array(training)
-# creazione dei set di train e di test: X - patterns, Y - intents
-train_x = list(training[:,0])
-train_y = list(training[:,1])
+# creation of train and test sets: X - patterns, Y - intents
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
 
-# creazione del modello
+# model creation
 model = Sequential()
 model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
@@ -75,7 +76,7 @@ model.add(Dense(len(train_y[0]), activation='softmax'))
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-#fitting and saving the model
+# fitting and saving the model
 hist = model.fit(np.array(train_x), np.array(train_y), epochs=300, batch_size=5, verbose=1)
 model.save('chatbot_model.h5', hist)
 
